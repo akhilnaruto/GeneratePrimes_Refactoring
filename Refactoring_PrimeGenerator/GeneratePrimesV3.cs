122916a3ffa292a637008c7ee6422fc8e65a67b5
akhilesh <akhil.munna02@gmail.com>
2016-02-11 19:22:17 +0530

    final refactoring

---
+++ Refactoring_PrimeGenerator/GeneratePrimesV3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Refactoring_PrimeGenerator
+{
+  public class GeneratePrimesV3
+  {
+    private static bool[] crossedOut;
+    private static int[] result;
+
+    public static int[] GeneratePrimeNumbers(int maxValue)
+    {
+      if (maxValue < 2)
+      {
+        return new int[0];
+      }
+
+      UnCrossIntegersUpTo(maxValue);
+      CrossOutMultiples();
+      PutUncrossedIntegersIntoResult();
+      return result;
+    }
+
+    private static void UnCrossIntegersUpTo(int maxValue)
+    {
+      crossedOut = new bool[maxValue + 1];
+      for (int i = 2; i < crossedOut.Length; i++)
+      {
+        crossedOut[i] = false;
+      }
+    }
+
+    private static void CrossOutMultiples()
+    {
+      for (int i = 2; i < Math.Sqrt(crossedOut.Length) + 1; i++)
+      {
+        if (NotCrossed(i)) 
+        {
+          CrossOutMultiplesOf(i);
+        }
+      }
+    }
+
+    private static void CrossOutMultiplesOf(int i)
+    {
+      for (int multiple = 2 * i; multiple < crossedOut.Length; multiple = multiple + i)
+      {
+        crossedOut[multiple] = true;
+      }
+    }
+
+    private static void PutUncrossedIntegersIntoResult()
+    {
+      result = new int[NumberOfUncrossedIntegers()];
+
+      for (int i = 2, j = 0; i < crossedOut.Length; i++)
+      {
+        if (NotCrossed(i)) 
+        {
+          result[j++] = i;
+        }
+      }
+    }
+
+    private static int NumberOfUncrossedIntegers()
+    {
+      int count = 0;
+      for (int i = 2; i < crossedOut.Length; i++)
+      {
+        if (NotCrossed(i))
+        {
+          count++;
+        }
+      }
+      return count;
+    }
+
+    private static bool NotCrossed(int i)
+    {
+      return crossedOut[i] == false;
+    }
+
+  }
+}
--- Refactoring_PrimeGenerator/TestGeneratePrimes.cs
+++ Refactoring_PrimeGenerator/TestGeneratePrimes.cs
@@ -7,16 +7,16 @@ namespace Refactoring_PrimeGenerator
     [Test]
     public void TestPrimes()
     {
-      int[] nullArray = GeneratePrimesV2.GeneratePrimeNumbers(0);
+      int[] nullArray = GeneratePrimesV3.GeneratePrimeNumbers(0);
       Assert.AreEqual(nullArray.Length, 0);
-      int[] minArray = GeneratePrimesV2.GeneratePrimeNumbers(2);
+      int[] minArray = GeneratePrimesV3.GeneratePrimeNumbers(2);
       Assert.AreEqual(minArray.Length, 1);
       Assert.AreEqual(minArray[0], 2);
-      int[] threeArray = GeneratePrimesV2.GeneratePrimeNumbers(3);
+      int[] threeArray = GeneratePrimesV3.GeneratePrimeNumbers(3);
       Assert.AreEqual(threeArray.Length, 2);
       Assert.AreEqual(threeArray[0], 2);
       Assert.AreEqual(threeArray[1], 3);
-      int[] centArray = GeneratePrimesV2.GeneratePrimeNumbers(100);
+      int[] centArray = GeneratePrimesV3.GeneratePrimeNumbers(100);
       Assert.AreEqual(centArray.Length, 25);
       Assert.AreEqual(centArray[24], 97);
     }
